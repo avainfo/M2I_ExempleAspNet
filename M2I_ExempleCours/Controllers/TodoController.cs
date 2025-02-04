1b5bfb7e0d783a2249642b137f2bbad40dd74738
anton <avainfoconseils@gmail.com>
2025-02-04 13:48:05 +0100

    feat: Correction TodoList

---
+++ M2I_ExempleCours/Controllers/TodoController.cs
@@ -0,0 +1,39 @@
+﻿using M2I_ExempleCours.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M2I_ExempleCours.Controllers;
+
+public class TodoController : Controller
+{
+	private static List<Todo> _todos = [];
+
+	// GET
+	public IActionResult Index()
+	{
+		return View(_todos);
+	}
+
+	[HttpPost]
+	public IActionResult AddTask(string title)
+	{
+		if (!string.IsNullOrEmpty(title) && !string.IsNullOrWhiteSpace(title))
+		{
+			Todo task = new Todo { Id = _todos.Count + 1, Title = title };
+			_todos.Add(task);
+		}
+
+		return RedirectToAction("Index");
+	}
+
+	[HttpPost]
+	public IActionResult CompleteTask(int id)
+	{
+		Todo? task = _todos.FirstOrDefault(todo => todo.Id == id);
+		if (task != null)
+		{
+			task.Completed = true;
+		}
+
+		return RedirectToAction("Index");
+	}
+}
---
+++ M2I_ExempleCours/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace M2I_ExempleCours.Models;
+
+public class Todo
+{
+	public required int Id { get; set; }
+	public required string Title { get; set; }
+	public bool Completed { get; set; }
+}
